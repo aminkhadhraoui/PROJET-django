2bcaed48e4f5615a4666301338c5a2dda1d311b0
mohamed amin khadhraoui <khadhraoui.amin.2022@gmail.com>
2023-12-04 21:54:30 +0100

    112 commit

---
+++ todo/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . views import TaskList,TaskDetail
+
+urlpatterns=[
+    path('',TaskList.as_view(), name='task'),
+    path('task/<int:pk>/',TaskDetail.as_view(), name="tasks-detail")
+
+]
+
+
+
